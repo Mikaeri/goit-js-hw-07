d0b8718e1796b35b82c131bb3c71e20d37da99dd
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-03-30 22:42:47 +0100

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,17 +1,6 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
 
 const gallery = document.querySelector(".js-container");
-// {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-//     description: 'Hokkaido Flower',
-//   }
-
 const markup = galleryItems.map(({preview, original, description}) => 
 `<li class="gallery__item">
    <a class="gallery__link" href="${original}">
@@ -20,7 +9,6 @@ const markup = galleryItems.map(({preview, original, description}) =>
 </li>`);
 
 gallery.insertAdjacentHTML('beforeend', markup.join(""));
-
 let lightBox = new SimpleLightbox('.gallery a',{    
     captions: true,
     captionsData: "alt",
